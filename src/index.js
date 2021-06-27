a69e04c8af1e5772975a3a206436d862f0759500
Lefucjusz <elektromarcin@gmail.com>
2021-06-27 17:44:05 +0200

    clock!

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ const bigFont = require('./services/bigFont');
 
 function start() {
     bigFont.init();
-    bigFont.writeChar(1, '0');
-    bigFont.writeChar(4, '1');
-    bigFont.writeChar(9, '2');	
+
+	setInterval(() => {
+		const date = new Date();
+		const hours = date.getHours().toString().padStart(2, '0');
+		const minutes = date.getMinutes().toString().padStart(2, '0');
+		const seconds = date.getSeconds().toString().padStart(2, '0');
+
+		bigFont.writeChar(1, hours[0]);
+		bigFont.writeChar(4, hours[1]);
+		bigFont.writeChar(8, minutes[0]);
+		bigFont.writeChar(11, minutes[1]);
+		bigFont.writeChar(15, seconds[0]);
+		bigFont.writeChar(18, seconds[1]);
+	}, 500);
 }
 
 start();
